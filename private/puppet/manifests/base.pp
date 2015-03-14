a1e921c5f50e884bf9cad6ca36135dd534e7b268
Ed Costello <ed.j.costello@gmail.com>
2015-03-14 19:20:30 +1300

    Install nginx and PHP using puppet

--- private/puppet/manifests/base.pp
+++ private/puppet/manifests/base.pp
@@ -1,3 +1,23 @@
 exec { 'apt-get update':
 	command => '/usr/bin/apt-get update',
 }
+
+package { 'nginx':
+	ensure => present,
+	require => Exec['apt-get update'],
+}
+
+package { 'php5-fpm':
+	ensure => present,
+	require => Exec['apt-get update'],
+}
+
+service { 'nginx':
+	ensure => running,
+	require => Package['nginx'],
+}
+
+service { 'php5-fpm':
+	ensure => running,
+	require => Package['nginx'],
+}
