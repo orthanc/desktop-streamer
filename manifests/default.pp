4d8c933accbac3898fa2bb9f3a680837cdb3f614
Ed Costello <ed.j.costello@gmail.com>
2015-03-14 23:29:53 +1300

    Initial script for starting Xvfb, currently not auto starting, not sure why

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,11 +1,21 @@
 exec { 'apt-get install xvfb':
-	command => '/usr/bin/apt-get install -y xvfb'
+	command => '/usr/bin/apt-get install -y xvfb ratpoison'
 }
 
 exec { 'apt-get install x11vnc':
 	command => '/usr/bin/apt-get install -y x11vnc'
 }
 
-exec { 'apt-get install ratpoison':
-	command => '/usr/bin/apt-get install -y ratpoison'
+file { 'headlessx-service':
+	path => '/etc/init.d/headlessx',
+	ensure => file,
+	source => 'puppet:///modules/headless-x-server/headlessx',
+}
+
+service { 'headlessx':
+	ensure => running,
+	require => [
+		Exec['apt-get install xvfb'],
+		File['headlessx-service'],
+	],
 }
