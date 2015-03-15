51eb476d1a99462dddda841dae68a0c79bdca4eb
Ed Costello <ed.j.costello@gmail.com>
2015-03-15 15:29:10 +1300

    Replace ratpoison wm with fluxbox for easier debugging

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,5 +1,5 @@
 exec { 'apt-get install xvfb':
-	command => '/usr/bin/apt-get install -y xvfb ratpoison'
+	command => '/usr/bin/apt-get install -y xvfb fluxbox'
 }
 
 exec { 'apt-get install x11vnc':
